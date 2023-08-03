7a58b13beb9d255cb032f907484876bca27534fb
Nguyễn Anh Tú <nguyentu@jvb-corp.com>
2023-08-03 19:10:44 +0700

    update code 3/8

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { createError } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import { HTTP_EXCEPTION_ERROR_CODE } from '../constants/errorMessage.js';
 
 export const register = async (req, res, next) => {
   try {
@@ -23,17 +25,35 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, 'User not found'));
+    if (!user)
+      return next(
+        createError(
+          HTTP_EXCEPTION_ERROR_CODE.USER_DOES_NOT_EXIST,
+          HTTP_EXCEPTION_ERROR_MESSAGES.USER_DOES_NOT_EXIST,
+        ),
+      );
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password,
     );
     if (!isPasswordCorrect)
-      return next(createError(400, 'Wrong password or username!'));
+    return next(
+      createError(
+        HTTP_EXCEPTION_ERROR_CODE.INVALID_PASSWORD,
+        HTTP_EXCEPTION_ERROR_MESSAGES.INVALID_PASSWORD,
+      ),
+    );
+    const token = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+    );
 
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import {
+    deleteUser,
+    getUser,
+    getUsers,
+    updateUser,
+  } from '../controllers/user.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+// Update
+router.put('/:id', verifyUser, updateUser);
+
+// Delete
+router.delete('/:id', verifyUser, deleteUser);
+
+// Get
+router.get('/:id', verifyUser, getUser);
+
+// Get all
+router.get('/', verifyAdmin, getUsers);
 
 export default router;
